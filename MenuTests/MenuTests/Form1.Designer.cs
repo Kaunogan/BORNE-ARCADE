62baaa6b5240ee24123df26bfc7f86b841532fa8
Alexandre <alexandre.gelin71@gmail.com>
2019-11-20 13:54:00 +0100

    creation dossier jeux + ajout du code du menu

---
+++ MenuTests/MenuTests/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MenuTests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelIndex2 = new System.Windows.Forms.Panel();
+            this.panelIndexm2 = new System.Windows.Forms.Panel();
+            this.panelIndex1 = new System.Windows.Forms.Panel();
+            this.panelIndexm1 = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelIndex2
+            // 
+            this.panelIndex2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelIndex2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelIndex2.Location = new System.Drawing.Point(460, 1);
+            this.panelIndex2.Name = "panelIndex2";
+            this.panelIndex2.Size = new System.Drawing.Size(353, 110);
+            this.panelIndex2.TabIndex = 0;
+            this.panelIndex2.UseWaitCursor = true;
+            // 
+            // panelIndexm2
+            // 
+            this.panelIndexm2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelIndexm2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelIndexm2.Location = new System.Drawing.Point(460, 569);
+            this.panelIndexm2.Name = "panelIndexm2";
+            this.panelIndexm2.Size = new System.Drawing.Size(353, 84);
+            this.panelIndexm2.TabIndex = 1;
+            // 
+            // panelIndex1
+            // 
+            this.panelIndex1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelIndex1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelIndex1.Location = new System.Drawing.Point(417, 101);
+            this.panelIndex1.Name = "panelIndex1";
+            this.panelIndex1.Size = new System.Drawing.Size(475, 107);
+            this.panelIndex1.TabIndex = 2;
+            // 
+            // panelIndexm1
+            // 
+            this.panelIndexm1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelIndexm1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelIndexm1.Location = new System.Drawing.Point(417, 467);
+            this.panelIndexm1.Name = "panelIndexm1";
+            this.panelIndexm1.Size = new System.Drawing.Size(475, 107);
+            this.panelIndexm1.TabIndex = 3;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainPanel.Location = new System.Drawing.Point(339, 194);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(629, 289);
+            this.mainPanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 656);
+            this.Controls.Add(this.panelIndexm1);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.panelIndex1);
+            this.Controls.Add(this.panelIndexm2);
+            this.Controls.Add(this.panelIndex2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelIndex2;
+        private System.Windows.Forms.Panel panelIndexm2;
+        private System.Windows.Forms.Panel panelIndex1;
+        private System.Windows.Forms.Panel panelIndexm1;
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
+
